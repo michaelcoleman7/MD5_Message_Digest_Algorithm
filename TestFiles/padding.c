f5487070e7dbb2fe267989dbd42ef535de594d86
michaelcoleman7 <michaelcoleman135@gmail.com>
2020-02-17 09:40:19 +0000

    added padding file to test files for SHA256

---
+++ TestFiles/padding.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+
+uint64_t nozerobytes(uint64_t nobits) {
+
+  uint64_t result = 512ULL - (nobits % 512ULL);
+
+  if (result < 65)
+    result += 512;
+
+  result -= 72;
+
+  return (result / 8ULL);
+}
+
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("Error: expected single filename as argument.\n");
+    return 1;
+  }
+
+  FILE *infile = fopen(argv[1], "rb");
+  if (!infile) {
+    printf("Error: couldn't open file %s.\n", argv[1]);
+    return 1;
+  }
+
+  uint8_t b;
+  uint64_t nobits;
+
+  for (nobits = 0; fread(&b, 1, 1, infile) == 1; nobits += 8) {
+    printf("%02" PRIx8, b);
+  }
+
+  printf("%02" PRIx8, 0x80); // Bits: 1000 0000
+
+  for (uint64_t i = nozerobytes(nobits); i > 0; i--)
+    printf("%02" PRIx8, 0x00);
+
+  printf("%016" PRIx64 "\n", nobits);
+
+  fclose(infile);
+
+  return 0;
+}
